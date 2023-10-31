523c15988c35441a5ee76cd8a5a7aa5fb6621dcb
Xinyang Ding <xinyangding@Shawns-MBP.lan>
2023-10-31 19:46:56 -0400

    modified file content: lemmy>crates>utils>build.rs

--- crates/utils/build.rs
+++ crates/utils/build.rs
@@ -1,9 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   rosetta_build::config()
-    .source("en", "translations/email/en.json")
-    .source("fi", "translations/email/fi.json")
-    .source("ko", "translations/email/ko.json")
-    .source("pt", "translations/email/pt.json")
+    .source("en", "../../../lemmy-ui/lemmy-translations/email/en.json")
+    .source("fi", "../../../lemmy-ui/lemmy-translations/email/fi.json")
+    .source("ko", "../../../lemmy-ui/lemmy-translations/email/ko.json")
+    .source("pt", "../../../lemmy-ui/lemmy-translations/email/pt.json")
     .fallback("en")
     .generate()?;
 
